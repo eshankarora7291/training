25eecbd56067788722c72763dbcefb4ed4052e2b
eshankarora7291 <30520353+eshankarora7291@users.noreply.github.com>
2017-09-14 12:42:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+﻿
+
+var ViewModel = function () {
+    var self = this;
+    self.users = ko.observableArray();
+    self.error = ko.observable();
+    self.team = ko.observable();
+
+    self.teams = ['MI', 'FE', 'Asset Answers', 'Foundation', 'Reliability'];
+
+    var baseUri = '/api/users/';
+    function ajaxHelper(uri, method, data)
+    {
+        self.error('');
+        return $.ajax(
+            {
+                type: method,
+                url: uri,
+                dataType: 'json',
+                contentType: 'application/json',
+                data: data ? JSON.stringify(data) : null
+
+            }).fail(function(jqXHR, textStatus, errorThrown)
+            {
+                self.error(errorThrown);
+            });
+    }
+
+    function onError(error) {
+        self.error('Error: ' + error.status + ' ' + error.statusText);
+    }
+    //fetch a list of users by teams and update the view model
+    self.getByTeam = function (team) {
+        self.team(team);
+        ajaxHelper(baseUri + team, 'GET', team).done(function (data) { self.users(data); });
+    };
+   // self.getByTeam(self.teams[1]);
+
+}
+ko.applyBindings(new ViewModel());
